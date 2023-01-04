d9233b7c88929249e411bb3c468e6c99c433fc80
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2023-01-04 09:16:17 +0100

    handlebuttons

--- src/GUI/Controller/MovieCollectionController.java
+++ src/GUI/Controller/MovieCollectionController.java
@@ -1,14 +1,38 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MovieCollectionController implements Initializable {
 
+    public Button addCategory;
+    public Button deleteCategory;
+    public Button rateMovie;
+    public Button addMovie;
+    public Button deleteMovie;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void handleAddCategory(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteCategory(ActionEvent actionEvent) {
+    }
+
+    public void handleRateMovie(ActionEvent actionEvent) {
+    }
+
+    public void handleAddMovie(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteMovie(ActionEvent actionEvent) {
     }
 }
