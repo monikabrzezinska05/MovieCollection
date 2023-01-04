cc9e314271b509a446d7354c59b0f2676601c977
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2023-01-04 11:01:02 +0100

    manager og model

--- src/BLL/MovieManager.java
+++ src/BLL/MovieManager.java
@@ -1,4 +1,10 @@
 package BLL;
 
+import DAL.db.MovieDAOV_DB;
+
 public class MovieManager {
+
+    private MovieDAOV_DB movieDAOV;
+
+    public MovieManager() { movieDAOV = new MovieDAOV_DB(); }
 }
--- src/GUI/Model/CategoryModel.java
+++ src/GUI/Model/CategoryModel.java
@@ -1,4 +1,18 @@
 package GUI.Model;
 
+import BE.Category;
+import BLL.CategoryManager;
+
 public class CategoryModel {
+    CategoryManager categoryManager;
+
+    public CategoryModel() throws Exception {
+        categoryManager = new CategoryManager();
+    }
+
+    public void createCategory(String name) throws Exception {
+        Category category = categoryManager.createCategory(name);
+
+    }
+
 }
