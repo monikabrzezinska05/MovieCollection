b4238d56fc46180e15f9920fcca5a85f1a4d83cb
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2023-01-04 08:49:18 +0100

    GUI

---
+++ src/GUI/Controller/MovieCollectionController.java
@@ -0,0 +1,14 @@
+package GUI.Controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MovieCollectionController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
