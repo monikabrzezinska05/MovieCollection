e03dcf666a3b4d73d20332754afe898976a9a99a
andersmadsen0511 <anders.bernhard.madsen@gmail.com>
2023-01-04 09:01:57 +0100

    Db connector and business entities.

---
+++ src/BE/Category.java
@@ -0,0 +1,21 @@
+package BE;
+
+public class Category {
+
+    private String name;
+
+    public Category (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/BE/Movie.java
@@ -0,0 +1,72 @@
+package BE;
+
+import java.util.Date;
+
+public class Movie {
+
+    private int id;
+    private String title;
+    private String filepath;
+    private Date lastWatched;
+    private int personalRating;
+    private int IMDBRating;
+
+
+    public Movie(int id, String title, String filepath, Date lastWatched, int personalRating, int IMDBRating) {
+        setId(id);
+        setTitle(title);
+        setFilepath(filepath);
+        setLastWatched(lastWatched);
+        setPersonalRating(personalRating);
+        setIMDBRating(IMDBRating);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+    public void setFilepath(String filepath) {
+        this.filepath = filepath;
+    }
+
+    public Date getLastWatched() {
+        return lastWatched;
+    }
+
+    public void setLastWatched(Date lastWatched) {
+        this.lastWatched = lastWatched;
+    }
+
+    public int getPersonalRating() {
+        return personalRating;
+    }
+
+    public void setPersonalRating(int personalRating) {
+        this.personalRating = personalRating;
+    }
+
+    public int getIMDBRating() {
+        return IMDBRating;
+    }
+
+    public void setIMDBRating(int IMDBRating) {
+        this.IMDBRating = IMDBRating;
+    }
+
+}
