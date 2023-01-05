47e0de38a2ae8ec8cba5e786cf0ac6c2aac7dd58
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2023-01-05 12:05:30 +0100

    ketchup

--- src/GUI/Controller/NewMovieController.java
+++ src/GUI/Controller/NewMovieController.java
@@ -5,7 +5,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -26,9 +29,18 @@ public class NewMovieController extends BaseController implements Initializable 
     }
 
     public void handleChooseFileButton(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("*.mp4", "*.mpeg4"));
+        fileChooser.setTitle("Choose MovieFile");
+
+        File file = fileChooser.showOpenDialog(chooseFileButton.getScene().getWindow());
+
     }
 
     public void handleCancelMovie(ActionEvent actionEvent) {
+        Stage stage = (Stage) cancelMovieButton.getScene().getWindow();
+        stage.close();
     }
 
     public void handleSaveMovie(ActionEvent actionEvent) {
