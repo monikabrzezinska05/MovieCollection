0a41522fedadaa9478268638efc81e1a6639d4b5
andersmadsen0511 <anders.bernhard.madsen@gmail.com>
2023-01-05 08:36:00 +0100

    Movies through the layers

--- src/GUI/Model/MovieModel.java
+++ src/GUI/Model/MovieModel.java
@@ -1,4 +1,24 @@
 package GUI.Model;
 
+import BE.Movie;
+import BLL.MovieManager;
+
 public class MovieModel {
+   MovieManager movieManager;
+
+    public MovieModel() throws Exception {
+        movieManager = new MovieManager();
+    }
+
+    public void createMovie(int id, String title, String filepath, java.sql.Date lastWatched, int personalRating, int IMDBRating) throws Exception {
+        movieManager.createMovie(id, title, filepath, lastWatched, personalRating, IMDBRating);
+    }
+
+    public void deleteMovie(Movie deletedMovie) throws Exception {
+        movieManager.deleteMovie(deletedMovie);
+    }
+
+
+
+
 }
