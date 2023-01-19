e8f88a82d38d4620d83a5f1db8dfb8dca103907f
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2023-01-19 10:40:09 +0100

    fxml Reminder

---
+++ src/GUI/Controller/ReminderController.java
@@ -0,0 +1,29 @@
+package GUI.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReminderController extends BaseController implements Initializable {
+    public Button deleteSelectedMovies;
+    public Button closeWindow;
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void handleDeleteSelectedMovies(ActionEvent actionEvent) {
+    }
+
+    public void handleCloseWindow(ActionEvent actionEvent) {
+    }
+}
